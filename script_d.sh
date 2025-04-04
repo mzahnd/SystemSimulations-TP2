3ad516768f43c021d1d465edd00eeb6423373853
Martín E. Zahnd <mzahnd@itba.edu.ar>
2025-04-04 01:44:31 -0300

    Keep one seed

--- script_d.sh
+++ script_d.sh
@@ -5,7 +5,7 @@ grid_sizes=(30 50 70 100)  # One smaller (30) and two larger (70, 100)
 steps=100000  # Ensure reaching steady state
 output_directory=./output
 probabilities=(0.02 0.04 0.06 0.08 0.10 0.12 0.14 0.16 0.18 0.20)
-seeds=(1743645648280 9876543210)
+seed=1743645648280
 
 # Clear the terminal
 clear
@@ -15,14 +15,12 @@ gradle clean build
 
 # Run the simulations with different grid sizes and seeds
 for grid_size in "${grid_sizes[@]}"; do
-  for seed in "${seeds[@]}"; do
-    echo "Running simulation for grid size $grid_size with seed $seed and probabilities: ${probabilities[*]}"
+  echo "Running simulation for grid size $grid_size with seed $seed and probabilities: ${probabilities[*]}"
 
-    gradle run --no-build-cache --rerun-tasks --args="\
-      --grid-size=${grid_size} \
-      --steps=${steps} \
-      --seed=${seed} \
-      --output-directory=${output_directory} $(printf ' --probability=%s' "${probabilities[@]}")"
-  done
+  gradle run --no-build-cache --rerun-tasks --args="\
+    --grid-size=${grid_size} \
+    --steps=${steps} \
+    --seed=${seed} \
+    --output-directory=${output_directory} $(printf ' --probability=%s' "${probabilities[@]}")"
 done
 
